ffcf1f76ab72138eddf23ac757cf481e1e540717
Guyabin1 <martinnyagaya60@gmail.com>
2023-06-15 06:19:46 -0700

    Hello, printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of artis useful\" - Dora Korpar,2015-10-19\n", stdout);
+	return (1)
+}
+                                                                    
